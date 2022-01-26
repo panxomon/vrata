915ecbc52cc038203bfe7d0493723448872b34da
Francisco Montoya <ext_jfranmontoya@falabella.cl>
2022-01-26 19:27:02 -0300

    agregado ejemplo de funcionamiento...

--- internal/command/command_test.go
+++ internal/command/command_test.go
@@ -2,6 +2,8 @@ package command_test
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
 	"vrata/internal/command"
 )
@@ -19,4 +21,38 @@ func Test_Create_Command(t *testing.T) {
 	}
 	command.Execute(context.TODO(), nil)
 
+}
+
+func Test_Command_Name(t *testing.T) {
+
+	data := &user{
+		name: "panxo",
+		age:  40,
+	}
+
+	command := command.NewCommand("crear usuario", crear_usuario)
+
+	if command.Name() != "crear usuario" {
+		t.Error("Expected command name to be 'crear usuario'")
+	}
+
+	command.Execute(context.TODO(), data)
+
+	fmt.Println(data)
+
+}
+
+type user struct {
+	name string
+	age  int
+}
+
+func crear_usuario(ctx context.Context, data interface{}) error {
+	usuario := data.(*user)
+
+	if usuario.name != "panxo" {
+		return errors.New("El nombre no es panxo")
+	}
+
+	return nil
 }
